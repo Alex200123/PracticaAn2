44bd7bddd5d47740b103e8536415a7ab205187cf
Alex200123 <100560843+Alex200123@users.noreply.github.com>
2022-06-30 09:27:33 +0300

    a

--- src/Model/RTS.java
+++ src/Model/RTS.java
@@ -1,5 +1,37 @@
 package Model;
 
-public class RTS {
+public class RTS implements Video_game_genre {
+
+	private String name;
+	private String Description;
+	private int score;
+	
+	@Override
+	public void Set(String name, String description, int score) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String Get_name() 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String Get_description() 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int Get_score() 
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
--- src/Model/Video_game_genre.java
+++ src/Model/Video_game_genre.java
@@ -2,4 +2,8 @@ package Model;
 
 public interface Video_game_genre {
 
+	public void Set(String name, String description, int score);
+	public String Get_name();
+	public String Get_description();
+	public int Get_score();
 }
